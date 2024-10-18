6620372f1d59692f3b519f7956796552bf7dca0a
John Ferlito <johnferlito73@gmail.com>
2024-10-18 00:19:41 -0400

    working on the POST

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 
 router.get('/', (req, res) => {
     fs.readFile('./db/db.json', 'utf-8', (err, data) => {
@@ -15,5 +16,23 @@ router.get('/', (req, res) => {
 });
 
 // Post request reference week 11 mini project in routes->tips.js
+router.post('/', (req, res)=>{
+    console.log(req.body);
+
+    const {title, text} = req.body;
+
+    if (req.body) {
+        const newRouter = {
+            title,
+            text,
+            id: uuidv4(),
+        };
+        readAndAppend(newRouter, './db/db.json');
+        res.json('New Note added successfully');
+    } else {
+        res.error('Error in adding a Note');
+    } 
+});
+
 
 module.exports = router;
